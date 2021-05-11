7b90acfb9bdc40a87a69715c9e2b1d3d0e9c3cd0
Oscar Otero <oom@oscarotero.com>
2021-05-11 23:29:38 +0200

    fixed url

--- tags.tmpl.js
+++ tags.tmpl.js
@@ -3,7 +3,7 @@ export const layout = "layouts/tag.njk";
 export default function* ({ search }) {
   for (const tag of search.tags()) {
     yield {
-      url: `tags/${tag}`,
+      url: `/tags/${tag}/`,
       title: `Tagged “${tag}”`,
       tag,
     };
