f8c2546c9cfb403186f1b8ef32419509f1080a08
Oscar Otero <oom@oscarotero.com>
2022-03-20 20:35:34 +0100

    updated lume

--- _config.ts
+++ _config.ts
@@ -6,7 +6,8 @@ import codeHighlight from "lume/plugins/code_highlight.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import resolveUrls from "lume/plugins/resolve_urls.ts";
-import gpm from "https://deno.land/x/gpm@v0.2.0/mod.ts";
+import netlifyCMS from "lume/plugins/netlify_cms.ts";
+import gpm from "https://deno.land/x/gpm@v0.4.1/mod.ts";
 
 const site = lume({
   location: new URL("https://example.com/"),
@@ -22,6 +23,7 @@ site
   .use(basePath())
   .use(slugifyUrls({ alphanumeric: false }))
   .use(resolveUrls())
+  .use(netlifyCMS({ netlifyIdentity: true }))
   .addEventListener(
     "beforeBuild",
     () => gpm(["oom-components/searcher"], "js/vendor"),
