2f988a6649bc55991dd4af858cef99c998be6b77
Oscar Otero <oom@oscarotero.com>
2022-03-20 20:36:46 +0100

    tweaks

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
-import Server from "https:/deno.land/x/lume@v1.5.1/core/server.ts";
+import Server from "https:/deno.land/x/lume/core/server.ts";
+import expires from "https:/deno.land/x/lume/middlewares/expires.ts";
 
 const server = new Server({
   port: 8000,
-  root: `${Deno.cwd()}/_site`
+  root: `${Deno.cwd()}/_site`,
 });
 
+server.use(expires());
 server.start();
 
 console.log("Listening on http://localhost:8000");
