0d78354d2ce8b832ab2a3d741f6d4823c67edc57
Oscar Otero <oom@oscarotero.com>
2022-07-20 22:01:02 +0200

    configure notfound middleware for deno deploy

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import Server from "lume/core/server.ts";
 import expires from "lume/middlewares/expires.ts";
+import notFound from "lume/middlewares/not_found.ts";
 
 const server = new Server({
   port: 8000,
@@ -7,6 +8,11 @@ const server = new Server({
 });
 
 server.use(expires());
+server.use(notFound({
+  root: `${Deno.cwd()}/_site`,
+  page404: "/404.html"
+}));
+
 server.start();
 
 console.log("Listening on http://localhost:8000");
